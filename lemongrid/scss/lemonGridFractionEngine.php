76b88f6a1876bfc3eea2a8f6cbb776b0121c0424
myke <michael@evoluted.net>
2013-11-04 16:44:56 +0000

    Rewrote a lot of it over the weekend

---
+++ lemongrid/scss/lemonGridFractionEngine.php
@@ -0,0 +1,62 @@
+<?php
+
+	$fractions = array();
+
+	for ($n=1; $n<=12; $n++) {
+	for ($d=1; $d<=12; $d++) {
+/*
+		if ($n <= $d)
+			$fractions[$n.'_'.$d] = floor($n / $d * 100000) / 1000;
+*/
+
+		if ($n <= $d) {
+			$percentbleh = floor($n / $d * 100000);
+			if (empty($fractions[$percentbleh]))
+				$fractions[$percentbleh][] = $n.'_'.$d;
+			else
+				$fractions[$percentbleh][] = $n.'_'.$d; 
+		}
+
+	}
+	}
+	
+/*
+	$fractionscomp = array();
+
+	foreach ($fractions as $fraction => $percent) {
+		$thisPercent = $percent;
+		$thisFraction = $percent;
+		$fractions 
+		foreach ($fractions as $fraction => $percent) {
+			if ($thisPercent == $percent && $thisFraction != $fraction)
+				echo "<br />.$fraction { ".$percent."% }";
+		}
+		
+
+	}
+
+
+	echo '<pre>';print_r($fractions);
+
+	foreach ($fractions as $fraction => $percent) {
+		echo "<br />.section--$fraction { ".$percent."% }";
+	}
+*/
+
+
+
+	echo '<pre>';print_r($fractions);
+
+	echo "<textarea style='width:500px;height:500px;'>/* Generated (don't edit) */";
+	foreach ($fractions as $percentbleh => $fractionarray) {
+		$count = 0;
+		foreach ($fractionarray as $fraction) {
+			if ($count+=0) echo ',';
+			echo "\n.section--$fraction";
+			$count++;
+		}
+		echo "\n{ width: ".($percentbleh/1000)."% }";
+	}
+
+
+?>
